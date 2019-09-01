f670e5f77c00e31d0728ba0a85a420ea7596eb5b
Bishal <bishalbhatta50@gmail.com>
2019-09-01 15:56:23 +0545

    created a new footer

---
+++ footer_2.php
@@ -0,0 +1,197 @@
+<html>
+
+<head>
+  <!--calling bootstrap 4 files and custom css-->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <!--calling bootstrap files-->
+  <script src="jquery/jquery-3.3.1.slim.min.js"></script>
+  <script src="jquery/popper.min.js"></script>
+  <script src="javascript/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+  <style type="text/css">
+  .footerLink {
+    color: white;
+  }
+  </style>
+</head>
+
+<body>
+  <!--started the main container-->
+  <div class="container d-none d-md-block" style="background-color:#1e2f25;">
+
+	 <!--created the row for the company name-->
+    <div class="row justify-content-center pt-3 pb-3">
+      <h3 class="footerLink">Company Name</h3>
+    </div>
+	
+	<div class="row justify-content-center">
+		<div class="col-xl-2 col-md-2">
+			<div class="d-flex flex-column">
+				<div>
+					<p class="font-weight-bold footerLink mb-2">Product-1</p>
+				</div>
+				<div class="footerLink">
+					<a href="#" class="footerLink">Link-1</a>
+				</div>
+				<div class="footerLink">
+					<a href="#" class="footerLink">Link-2</a>
+				</div>
+				<div class="footerLink">
+					<a href="#" class="footerLink">Link-3</a>
+				</div>
+				<div class="footerLink">
+					<a href="#" class="footerLink">Link-4</a>
+				</div>
+				<div class="footerLink">
+					<a href="#" class="footerLink">Link-5</a>
+				</div>
+			</div>
+		</div>
+		<div class="col-xl-2 col-md-2">
+			<div class="d-flex flex-column">
+				<div>
+					<p class="font-weight-bold footerLink mb-2">Product-2</p>
+				</div>
+				<div class="footerLink">
+					<a href="#" class="footerLink">Link-1</a>
+				</div>
+				<div class="footerLink">
+					<a href="#" class="footerLink">Link-2</a>
+				</div>
+				<div class="footerLink">
+					<a href="#" class="footerLink">Link-3</a>
+				</div>
+				<div class="footerLink">
+					<a href="#" class="footerLink">Link-4</a>
+				</div>
+				<div class="footerLink">
+					<a href="#" class="footerLink">Link-5</a>
+				</div>
+			</div>
+		</div>
+		<div class="col-xl-2 col-md-2">
+			<div class="d-flex flex-column">
+				<div>
+					<p class="font-weight-bold footerLink mb-2">Product-3</p>
+				</div>
+				<div class="footerLink">
+					<a href="#" class="footerLink">Link-1</a>
+				</div>
+				<div class="footerLink">
+					<a href="#" class="footerLink">Link-2</a>
+				</div>
+				<div class="footerLink">
+					<a href="#" class="footerLink">Link-3</a>
+				</div>
+				<div class="footerLink">
+					<a href="#" class="footerLink">Link-4</a>
+				</div>
+				<div class="footerLink">
+					<a href="#" class="footerLink">Link-5</a>
+				</div>
+			</div>
+		</div>
+		<div class="col-xl-2 col-md-2">
+			<div class="d-flex flex-column">
+				<div>
+					<p class="font-weight-bold footerLink mb-2">Product-4</p>
+				</div>
+				<div class="footerLink">
+					<a href="#" class="footerLink">Link-1</a>
+				</div>
+				<div class="footerLink">
+					<a href="#" class="footerLink">Link-2</a>
+				</div>
+				<div class="footerLink">
+					<a href="#" class="footerLink">Link-3</a>
+				</div>
+				<div class="footerLink">
+					<a href="#" class="footerLink">Link-4</a>
+				</div>
+				<div class="footerLink">
+					<a href="#" class="footerLink">Link-5</a>
+				</div>
+			</div>
+		</div>
+		<div class="col-xl-2 col-md-2">
+			<div>
+				<p class="font-weight-bold footerLink mb-2">Follow us:</p>
+			</div>
+			<div>
+				<a href="#">
+					<button class="btn " type="button" style="color:white">
+						<i class="fa fa-facebook-square fa-lg"></i>
+					  </button>
+				</a>
+			</div>
+			<div>
+				<a href="#">
+					<button class="btn " type="button" style="color:white">
+						<i class="fa fa-instagram fa-lg"></i>
+					  </button>
+				</a>
+			</div>
+			<div>
+				<a href="#">
+					<button class="btn " type="button" style="color:white">
+						<i class="fa fa-skype fa-lg"></i>
+					  </button>
+				</a>
+			</div>
+			<div>
+				<a href="#">
+					<button class="btn " type="button" style="color:white">
+						<i class="fa fa-linkedin-square fa-lg"></i>
+					  </button>
+				</a>
+			</div>
+			<div>
+				<a href="#">
+					<button class="btn " type="button" style="color:white">
+						<i class="fa fa-google-plus fa-lg"></i>
+					  </button>
+				</a>
+			</div>
+		</div>
+	</div>
+	
+	<div class="row justify-content-center">
+		 <!--Adding company address, email, contact no-->
+		<div class="d-flex flex-row pb-3 justify-content-center align-items-center">
+		  <div>
+			<h6 class="footerLink mb-2 fa fa-map-marker"> Nepal, Kathmandu</h6>
+		  </div>
+		  <div class="pl-3 pr-3">
+			<h6 class="footerLink mb-2 fa fa-mobile-phone"> +977-1234567890</h6>
+		  </div>
+		  <div>
+			<h6 class="footerLink mb-2 fa fa-envelope"> abc@gmail.com</h6>
+		  </div>
+		</div>
+
+	</div>
+		<!--creating the card to keep privacy policy part-->
+
+	<div class="row mb-2 justify-content-center" style="background-color:white">
+			<div class="col-xl-2 col-lg-2 col-md-3 col-sm-4 mt-1">
+				<p class="font-weight-bold fa fa-copyright">Copyright 2019</p>
+			</div>
+			<div class="col-xl-1 col-lg-1 col-md-2 col-sm-2">
+				<a href="#" style="color:black;">Terms</a>
+			</div>
+			<div class="col-xl-1 col-lg-1 col-md-2 col-sm-2">
+				<a href="#" style="color:black;">Terms</a>
+			</div>
+			<div class="col-xl-1 col-lg-1 col-md-2 col-sm-2">
+				<a href="#" style="color:black;">Terms</a>
+			</div>
+	</div>
+
+  </div>
+</body>
+
+</html>
