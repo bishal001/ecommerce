22ab0fdfc30d0c6330b32f06a6a1554b4d1bc47a
Bishal <bishalbhatta50@gmail.com>
2019-09-05 17:47:14 +0545

    added the button for different register module

--- signup.php
+++ signup.php
@@ -4,6 +4,12 @@
 <?php include'nav.php'?>
 </head>
 <body>
-  
+  <div class="container">
+    <div class="d-flex flex-row">
+      <button type="submit" class="btn btn-dangers">Register As A Customer</button>
+      <button type="submit" class="btn btn-dangers ml-3 mr-3">Register As A Volume Buyer</button>
+      <button type="submit" class="btn btn-dangers">Register As A Supplier</button>
+    </div>
+  </div>
 </body>
 </html>
