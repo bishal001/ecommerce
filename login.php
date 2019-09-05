c4024adbabe0c6601f5666b49fa56480afdbb472
Bishal <bishalbhatta50@gmail.com>
2019-09-05 15:59:32 +0545

    added the action

--- login.php
+++ login.php
@@ -6,14 +6,14 @@
 <body>
   <div class="container" style="display: flex; justify-content: center; align-items: center;">
     <div class="shadow p-3 bg-white" style="width:40%">
-      <form action="#">
+      <form action="_home.php">
         <div class="form-group">
           <label for="email">Email address:</label>
-          <input type="email" class="form-control" placeholder="example@gmail.com" id="email" style="width:50%" required>
+          <input type="email" class="form-control" placeholder="example@gmail.com" id="email" style="width:70%" required>
         </div>
         <div class="form-group">
           <label for="pwd">Password:</label>
-          <input type="password" class="form-control" placeholder="password" id="pwd" style="width:50%" required>
+          <input type="password" class="form-control" placeholder="password" id="pwd" style="width:70%" required>
         </div>
         <button type="submit" class="btn btn-dangers">Login</button>
       </form>
