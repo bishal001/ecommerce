6848595f5ede5f4015eae69b3d5c407b4487d626
Bishal <bishalbhatta50@gmail.com>
2019-09-05 15:54:46 +0545

    added form with email, password and submit button. used the bootstrap class to make form responsive

--- login.php
+++ login.php
@@ -1,10 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <link rel="stylesheet" href="css/owl.carousel.min.css">
-  <link rel="stylesheet" href="css/owl.theme.default.min.css">
+<?php include'nav.php'?>
 </head>
 <body>
-  
+  <div class="container" style="display: flex; justify-content: center; align-items: center;">
+    <div class="shadow p-3 bg-white" style="width:40%">
+      <form action="#">
+        <div class="form-group">
+          <label for="email">Email address:</label>
+          <input type="email" class="form-control" placeholder="example@gmail.com" id="email" style="width:50%" required>
+        </div>
+        <div class="form-group">
+          <label for="pwd">Password:</label>
+          <input type="password" class="form-control" placeholder="password" id="pwd" style="width:50%" required>
+        </div>
+        <button type="submit" class="btn btn-dangers">Login</button>
+      </form>
+    </div>
+  </div>
 </body>
 </html>
