6beb812d651163f561cae72935ec2c19567eb684
Bishal <bishalbhatta50@gmail.com>
2019-09-06 11:41:20 +0545

    added heading of the register page

--- register_cust_buyer.php
+++ register_cust_buyer.php
@@ -1,12 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+  <?php include'nav.php'?>
 </head>
+
 <body>
   
+<div class="container">
+  <div class="row">
+    <h4>Register Account</h4>
+  </div>
+  <div class="row">
+    <div class="shadow p-3 bg-white" style="width:40%">
+        <form action="_home.php">
+          <div class="form-group">
+            <label for="email">Email address:</label>
+            <input type="email" class="form-control" placeholder="example@gmail.com" id="email" style="width:70%" required>
+          </div>
+          <div class="form-group">
+            <label for="pwd">Password:</label>
+            <input type="password" class="form-control" placeholder="password" id="pwd" style="width:70%" required>
+          </div>
+          <button type="submit" class="btn btn-dangers">Login</button>
+        </form>
+    </div>
+  </div>
+</div>
 </body>
+
 </html>
--- signup.php
+++ signup.php
@@ -6,8 +6,8 @@
 <body>
   <div class="container">
     <div class="d-flex flex-row justify-content-center">
-      <a href="#" class="btn btn-dangers">Register As A Customer</a>
-      <a href="#" class="btn btn-dangers ml-3 mr-3">Register As A Volume Buyer</a>
+      <a href="register_cust_buyer.php" class="btn btn-dangers">Register As A Customer</a>
+      <a href="register_cust_buyer.php" class="btn btn-dangers ml-3 mr-3">Register As A Volume Buyer</a>
       <a href="#" class="btn btn-dangers">Register As A Supplier</a>
     </div>
   </div>
