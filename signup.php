0d5968cce03f1939b7346dae9f2f53c40e9858b1
Bishal <bishalbhatta50@gmail.com>
2019-09-08 14:46:25 +0545

    changed the page layout

--- signup.php
+++ signup.php
@@ -6,7 +6,7 @@
 <body>
   <div class="container">
     <div class="row justify-content-center mx-auto">
-      <div class="col-xl-3 col-lg-4 col-md-4 col-sm-8 mb-3">
+      <div class="col-xl-3 col-lg-4 col-md-5 col-sm-8 mb-3">
         <a href="register_cust_buyer.php" class="btn btn-dangers" style="width:230px">Register As A Customer</a>
       </div>
       <div class="col-xl-3 col-lg-4 col-md-4 col-sm-8 mb-3">
