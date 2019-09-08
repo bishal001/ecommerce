2095a9a8dc74a1db795f55a6b11170ffe1a0fc4f
Bishal <bishalbhatta50@gmail.com>
2019-09-08 13:04:42 +0545

    changed the background of the form, changed the color if the text field and the label

--- register_cust_buyer.php
+++ register_cust_buyer.php
@@ -3,6 +3,28 @@
 
 <head>
   <?php include'nav.php'?>
+  <style>
+  .bg{
+    background-image: url("images/form_background.jpg");
+    background-repeat: no-repeat;
+      background-color: #cccccc;
+      background-size: cover;
+      background-position: center center;
+      background-attachment: fixed;
+      width:700px;
+      height:300px;
+  }
+  .text{
+    color: white;
+  }
+  .background{
+    background-color: #49ff46;
+    color: white;
+    width:70%;
+    box-sizing: border-box;
+    border: none;
+  }
+</style>
 </head>
 
 <body>
@@ -12,17 +34,17 @@
     <h4>Register Account</h4>
   </div>
   <div class="row justify-content-center">
-    <div class="shadow p-3 bg-white" style="width:70%">
+    <div class="shadow p-3 bg-white bg" style="width:70%">
         <form action="_home.php">
         <div class="row mb-3">
           <div class="col-xl-6">
-            <label for="fname">First Name:</label>
-            <input type="text" class="form-control" placeholder="First Name" id="fname" 
+            <label for="fname" class="text">First Name:</label>
+            <input type="text" class="form-control background" placeholder="First Name" id="fname" 
               required>
           </div>
           <div class="col-xl-6">
-            <label for="lname">Last Name:</label>
-            <input type="text" class="form-control" placeholder="Last Name" id="lname" required>
+            <label for="lname" class="text">Last Name:</label>
+            <input type="text" class="form-control background" placeholder="Last Name" id="lname" required>
           </div>
         </div>
           <button type="submit" class="btn btn-dangers">Login</button>
