19e73c4051b8a1cf515ba4a291056ecc4e989f56
Bishal <bishalbhatta50@gmail.com>
2019-09-09 11:53:19 +0545

    created the register page for the supplier seller information

--- register_supplier_personalDetail.php
+++ register_supplier_personalDetail.php
@@ -55,7 +55,7 @@
     <div class="row justify-content-center">
       <div class="shadow p-1 bg-white bg" style="width:70%">
         <!-- creatin a register form and setting the action-->
-        <form action="_home.php">
+        <form action="">
           <!-- creating the personial detail section -->
           <div class="row mb-3">
             <div class="col-xl-6">
