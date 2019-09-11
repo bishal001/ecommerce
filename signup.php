5b067971a374f5dba9bd1a86a8289db926f942e8
Bishal <bishalbhatta50@gmail.com>
2019-09-11 14:22:00 +0545

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,17 +1,16 @@
-
-  <div class="container shadow-lg bg-white">
-    <?php include'nav.php'?>    
-    <div class="row justify-content-center mx-auto">
-      <div class="col-xl-3 col-lg-4 col-md-5 col-sm-8 mb-3">
-        <a href="register_cust_buyer.php" class="btn btn-dangers" style="width:230px">Register As A Customer</a>
-      </div>
-      <div class="col-xl-3 col-lg-4 col-md-4 col-sm-8 mb-3">
-        <a href="register_cust_buyer.php" class="btn btn-dangers" style="width:230px">Register As A Volume Buyer</a>
-      </div>
-      <div class="col-xl-3 col-lg-4 col-md-4 col-sm-8">
-        <a href="register_supplier_personalDetail.php" class="btn btn-dangers" style="width:230px">Register As A Supplier</a>
-      </div>
+<div class="container shadow-lg bg-white">
+  <?php include'nav.php'?>    
+  <div class="row justify-content-center mx-auto">
+    <div class="col-xl-3 col-lg-4 col-md-5 col-sm-8 mb-3">
+      <a href="register_cust_buyer.php" class="btn btn-dangers" style="width:230px">Register As A Customer</a>
     </div>
-    <hr>
-  <?php include'footer_2.php'?>
+    <div class="col-xl-3 col-lg-4 col-md-4 col-sm-8 mb-3">
+      <a href="register_cust_buyer.php" class="btn btn-dangers" style="width:230px">Register As A Volume Buyer</a>
+    </div>
+    <div class="col-xl-3 col-lg-4 col-md-4 col-sm-8">
+      <a href="register_supplier_personalDetail.php" class="btn btn-dangers" style="width:230px">Register As A Supplier</a>
+    </div>
   </div>
+  <hr>
+  <?php include'footer_2.php'?>
+</div>
