3d538ee81a07d8b085f99864df41afbc23e787ac
Bishal <bishalbhatta50@gmail.com>
2019-09-11 14:55:30 +0545

    added forget password and signup button and also added arrow icon on the buttons

--- login.php
+++ login.php
@@ -1,21 +1,27 @@
 <div class="container shadow-lg bg-white">
   <?php include'nav.php'?>
-    <div class="row justify-content-center">
+  <div class="row justify-content-center">
     <div class="shadow bg p-3 bg-white">
       <!-- creating a form -->
       <form action="_home.php">
         <div class="row no-gutters">
           <label for="email" class="text">Email address:</label>
-          <input type="email" class="form-control background placeholdColor" placeholder="example@gmail.com" id="email" required>
+          <input type="email" class="form-control background placeholdColor" placeholder="example@gmail.com" id="email"
+            required>
         </div>
         <div class="row no-gutters">
           <label for="pwd" class="text">Password:</label>
-          <input type="password" class="form-control background placeholdColor" placeholder="password" id="pwd" required>
+          <input type="password" class="form-control background placeholdColor" placeholder="password" id="pwd"
+            required>
         </div>
-        <button type="submit" class="btn btn-dangers mt-3">Login</button>
+        <div class="d-flex flex-row justify-content-center">
+          <button type="submit" class="btn btn-dangers mt-3 font-weight-bold">Login <i class="fa fa-arrow-right ml-1"></i> </button>
+          <button type="submit" class="btn btn-dangers mt-3 font-weight-bold ml-2">Forget Password <i class="fa fa-arrow-right ml-1"></i> </button>
+          <a href="signup.php" class="btn btn-dangers mt-3 font-weight-bold ml-2" > Signup<i class="fa fa-arrow-right ml-1"></i> </a>
+        </div>
       </form>
     </div>
-    </div>
-    <hr>
-    <?php include'footer_2.php'?>
   </div>
+  <hr>
+  <?php include'footer_2.php'?>
+</div>
