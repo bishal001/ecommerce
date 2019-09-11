4e9b4709667e5f37f040f2e58f4c8effcfbf10ab
Bishal <bishalbhatta50@gmail.com>
2019-09-11 15:41:31 +0545

    a

--- cart.php
+++ cart.php
@@ -1,102 +1,101 @@
-
-  <div class="container shadow-lg bg-white">
-    <?php include'nav.php'?>
-    <div class="row">
-      <div class="col-xl-9">
-        <div class="shadow-sm bg-white">
-          <!-- creating the table to store the cart detail -->
-          <table class="table table-borderless">
-            <!-- table heading -->
-            <tr class="table-secondary">
-              <th>Product Name</th>
-              <th>Quantity</th>
-              <th>Price</th>
-              <th>Edit</th>
-              <th></th>
-            </tr>
-            <!-- table body -->
-            <tr>
-              <td>
-                <!-- this row contains the product name and image -->
-                <div class="d-flex flex-row">
-                  <img src="http://placehold.it/90x90" class="img-fluid mr-3">
-                  <p>Apple</p>
-                </div>
-              </td>
-              <td>
-                <!-- contains the quantity detail -->
-                <div class="d-flex flex-row">
-                  <i class="btn fa fa-minus-circle fa-lg" onclick="decrease()"></i>
-                  <input type="text" size="1" id="quantity" class="calculator-input" value="0"
-                    onkeypress="return event.charCode >= 48 && event.charCode <= 57">
-                  <i class=" btn fa fa-plus-circle fa-lg" onclick="increase()"></i>
-                </div>
-              </td>
-              <td>
-                <!-- contans the price detail -->
-                <p>$10</p>
-              </td>
-              <td>
-                <!-- contains the icons for the edit -->
-                <div class="d-flex flex-row mt-1">
-                  <i class="fa fa-trash fa-lg mr-3"></i>
-                  <i class="fa fa-refresh fa-lg"></i>
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <!-- this row contains the product name and image -->
-                <div class="d-flex flex-row">
-                  <img src="http://placehold.it/90x90" class="img-fluid mr-3">
-                  <p>Orange</p>
-                </div>
-              </td>
-              <td>
-                <!-- contains the quantity detail -->
-                <div class="d-flex flex-row">
-                  <i class="btn fa fa-minus-circle fa-lg" onclick="decrease()"></i>
-                  <input type="text" size="1" id="quantity" class="calculator-input" value="0"
-                    onkeypress="return event.charCode >= 48 && event.charCode <= 57">
-                  <i class=" btn fa fa-plus-circle fa-lg" onclick="increase()"></i>
-                </div>
-              </td>
-              <td>
-                <!-- contans the price detail -->
-                <p>$10</p>
-              </td>
-              <td>
-                <!-- contains the icons for the edit -->
-                <div class="d-flex flex-row mt-1">
-                  <i class="fa fa-trash fa-lg mr-3"></i>
-                  <i class="fa fa-refresh fa-lg"></i>
-                </div>
-              </td>
-            </tr>
-          </table>
+<div class="container shadow-lg bg-white">
+  <?php include'nav.php'?>
+  <div class="row">
+    <div class="col-xl-9">
+      <div class="shadow-sm bg-white">
+        <!-- creating the table to store the cart detail -->
+        <table class="table table-borderless">
+          <!-- table heading -->
+          <tr class="table-secondary">
+            <th>Product Name</th>
+            <th>Quantity</th>
+            <th>Price</th>
+            <th>Edit</th>
+            <th></th>
+          </tr>
+          <!-- table body -->
+          <tr>
+            <td>
+              <!-- this row contains the product name and image -->
+              <div class="d-flex flex-row">
+                <img src="http://placehold.it/90x90" class="img-fluid mr-3">
+                <p>Apple</p>
+              </div>
+            </td>
+            <td>
+              <!-- contains the quantity detail -->
+              <div class="d-flex flex-row">
+                <i class="btn fa fa-minus-circle fa-lg" onclick="decrease()"></i>
+                <input type="text" size="1" id="quantity" class="calculator-input" value="0"
+                  onkeypress="return event.charCode >= 48 && event.charCode <= 57">
+                <i class=" btn fa fa-plus-circle fa-lg" onclick="increase()"></i>
+              </div>
+            </td>
+            <td>
+              <!-- contans the price detail -->
+              <p>$10</p>
+            </td>
+            <td>
+              <!-- contains the icons for the edit -->
+              <div class="d-flex flex-row mt-1">
+                <i class="fa fa-trash fa-lg mr-3"></i>
+                <i class="fa fa-refresh fa-lg"></i>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <!-- this row contains the product name and image -->
+              <div class="d-flex flex-row">
+                <img src="http://placehold.it/90x90" class="img-fluid mr-3">
+                <p>Orange</p>
+              </div>
+            </td>
+            <td>
+              <!-- contains the quantity detail -->
+              <div class="d-flex flex-row">
+                <i class="btn fa fa-minus-circle fa-lg" onclick="decrease()"></i>
+                <input type="text" size="1" id="quantity" class="calculator-input" value="0"
+                  onkeypress="return event.charCode >= 48 && event.charCode <= 57">
+                <i class=" btn fa fa-plus-circle fa-lg" onclick="increase()"></i>
+              </div>
+            </td>
+            <td>
+              <!-- contans the price detail -->
+              <p>$10</p>
+            </td>
+            <td>
+              <!-- contains the icons for the edit -->
+              <div class="d-flex flex-row mt-1">
+                <i class="fa fa-trash fa-lg mr-3"></i>
+                <i class="fa fa-refresh fa-lg"></i>
+              </div>
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+    <!-- shows the totla price and checkout button -->
+    <div class="col-xl-3">
+      <div class="card">
+        <div class="card-header">
+          <h5>Order Total</h5>
+        </div>
+        <div class="card-body">
+          <div class="row mx-auto">
+            <p class="font-weight-bold">Sub-Total: $10</p>
+          </div>
+          <div class="row mx-auto">
+            <p class="font-weight-bold">VAT: 0</p>
+          </div>
+          <div class="row mx-auto">
+            <p class="font-weight-bold">Grand-Total: $10</p>
+          </div>
+          <div>
+            <button class="btn btn-dangers my-2" type="button" style="color:white">Checkout</button>
+          </div>
         </div>
       </div>
-      <!-- shows the totla price and checkout button -->
-      <div class="col-xl-3">
-				<div class="card">
-          <div class="card-header">
-            <h5>Order Total</h5>
-          </div>
-          <div class="card-body">
-            <div class="row mx-auto">
-              <p class="font-weight-bold">Sub-Total: $10</p>
-            </div>
-            <div class="row mx-auto">
-              <p class="font-weight-bold">VAT: 0</p>
-            </div>
-            <div class="row mx-auto">
-              <p class="font-weight-bold">Grand-Total: $10</p>
-            </div>
-            <div>
-              <button class="btn btn-dangers my-2" type="button" style="color:white">Checkout</button>
-            </div>
-          </div>
-        </div>
     </div>
   </div>
   <hr>
--- contact.php
+++ contact.php
@@ -1,36 +1,35 @@
-
-  <div class="container shadow-lg bg-white">
-    <?php include'nav.php'?>
-    <div class="row">
-      <div class="col-xl-5 col-lg-6 mb-3">
-        <div class="shadow p-2">
-          <div class="d-flex flex-column">
-            <div>
-              <p class="font-weight-bold">Name:</p> <input type="text" size="30">
-            </div>
-            <div class="mt-3">
-              <p class="font-weight-bold">E-mail:</p> <input type="text" size="30">
-            </div>
-            <div class="mt-3">
-              <p class="font-weight-bold">Phone No:</p> <input type="text" size="30">
-            </div>
-            <div class="mt-3">
-              <p class="font-weight-bold">Comment:</p>
-              <textarea rows="5" cols="45"></textarea><br>
-              <input type="Submit" class="my-2 btn btn-dangers float-right">
-            </div>
+<div class="container shadow-lg bg-white">
+  <?php include'nav.php'?>
+  <div class="row">
+    <div class="col-xl-5 col-lg-6 mb-3">
+      <div class="shadow p-2">
+        <div class="d-flex flex-column ml-2">
+          <div>
+            <p class="font-weight-bold">Name:</p> <input type="text" size="30">
+          </div>
+          <div class="mt-3">
+            <p class="font-weight-bold">E-mail:</p> <input type="text" size="30">
+          </div>
+          <div class="mt-3">
+            <p class="font-weight-bold">Phone No:</p> <input type="text" size="30">
+          </div>
+          <div class="mt-3">
+            <p class="font-weight-bold">Comment:</p>
+            <textarea rows="5" cols="55"></textarea><br>
+            <input type="Submit" class="my-2 btn btn-dangers">
           </div>
         </div>
       </div>
-      <!-- adding the google map -->
-      <div class="col-xl-7 col-lg-6 text-center">
-        <div class="map-responsive">
-          <iframe src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA0s1a7phLN0iaD6-UE7m4qP-z21pH0eSc&q=Eiffel+Tower+Paris+France" width="100%" height="400"></iframe>
-        </div>
-      </div>
-      
     </div>
-
-    <hr>
-    <?php include'footer_2.php'?>
+    <!-- adding the google map -->
+    <div class="col-xl-7 col-lg-6 text-center">
+      <div class="map-responsive">
+        <iframe
+          src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA0s1a7phLN0iaD6-UE7m4qP-z21pH0eSc&q=Eiffel+Tower+Paris+France"
+          width="100%" height="400"></iframe>
+      </div>
+    </div>
   </div>
+  <hr>
+  <?php include'footer_2.php'?>
+</div>
