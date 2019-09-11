4304d2eec45be127509b702f759f7e7c021ee428
Bishal <bishalbhatta50@gmail.com>
2019-09-11 12:14:46 +0545

    created the hedaer page and kept the containts of the heeader in it

---
+++ header.php
@@ -0,0 +1,17 @@
+<html>
+
+<head>
+  <!--calling bootstrap 4 files and custom css-->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/nav_1.css">
+  <!--calling bootstrap files-->
+  <script src="jquery/jquery-3.3.1.slim.min.js"></script>
+  <script src="jquery/popper.min.js"></script>
+  <script src="javascript/bootstrap.min.js"></script>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+
+</html>
--- nav.php
+++ nav.php
@@ -1,22 +1,6 @@
-<html>
-
-<head>
-  <!--calling bootstrap 4 files and custom css-->
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-  <link rel="stylesheet" href="css/bootstrap.min.css">
-  <link rel="stylesheet" href="css/nav_1.css">
-  <!--calling bootstrap files-->
-  <script src="jquery/jquery-3.3.1.slim.min.js"></script>
-  <script src="jquery/popper.min.js"></script>
-  <script src="javascript/bootstrap.min.js"></script>
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-</head>
 
 <body>
-
-  <!--creating navigation bar-->
+ <!--creating navigation bar-->
   <nav class="navbar navbar-expand-xl navbar-dark sticky-top">
     <div class="container">
       <!--creating toggler button in nav bar-->
@@ -70,7 +54,4 @@
       </div>
     </div>
   </nav>
-
-</body>
-
-</html>
+  </body>
