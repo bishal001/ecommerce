b454b0390cc7b1df20caf3b6a490902b59ba1cd0
Bishal <bishalbhatta50@gmail.com>
2019-09-11 15:59:48 +0545

    Update contact.php

--- contact.php
+++ contact.php
@@ -2,7 +2,7 @@
   <?php include'nav.php'?>
   <div class="row">
     <div class="col-xl-5 col-lg-6 mb-3">
-      <div class="shadow p-2">
+      <div class="shadow ">
         <div class="d-flex flex-column ml-2">
           <div>
             <p class="font-weight-bold">Name:</p> <input type="text" size="30">
@@ -15,7 +15,7 @@
           </div>
           <div class="mt-3">
             <p class="font-weight-bold">Comment:</p>
-            <textarea rows="5" cols="55"></textarea><br>
+            <textarea rows="5" cols="50"></textarea><br>
             <input type="Submit" class="my-2 btn btn-dangers">
           </div>
         </div>
@@ -26,7 +26,7 @@
       <div class="map-responsive">
         <iframe
           src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA0s1a7phLN0iaD6-UE7m4qP-z21pH0eSc&q=Eiffel+Tower+Paris+France"
-          width="100%" height="400"></iframe>
+          width="100%" height="490" style="border:none;"></iframe>
       </div>
     </div>
   </div>
