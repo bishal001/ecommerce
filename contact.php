1c6f0e58a0eca86ada1c3e74ef0779a41186f676
Bishal <bishalbhatta50@gmail.com>
2019-09-12 13:05:19 +0545

    added the map section

--- contact.php
+++ contact.php
@@ -1,7 +1,15 @@
 <div class="container shadow-lg bg-white">
   <?php include'nav.php'?>
-    <div class="row shadow-sm justify-content-center" style="background-color: green; color:white">
+    <div class="row shadow-sm justify-content-center mb-2" style="background-color: green; color:white">
       <p class="mt-2">Got A QUESTION OR INQUIRY?</p>
+    </div>
+    <!-- this div contains the map section -->
+    <div class="row">
+      <div class="map-responsive">
+        <iframe
+          src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA0s1a7phLN0iaD6-UE7m4qP-z21pH0eSc&q=Eiffel+Tower+Paris+France"
+          class="w-100" height="490" style="border:none;"></iframe>
+      </div>
     </div>
   <hr>
   <?php include'footer_2.php'?>
