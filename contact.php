65b57d2fb61339f018d1f52c46f6411ac9ad214f
Bishal <bishalbhatta50@gmail.com>
2019-09-12 13:14:30 +0545

    made the map responsive

--- contact.php
+++ contact.php
@@ -4,12 +4,10 @@
       <p class="mt-2">Got A QUESTION OR INQUIRY?</p>
     </div>
     <!-- this div contains the map section -->
-    <div class="row">
-      <div class="map-responsive">
+    <div class="row map-responsive">
         <iframe
           src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA0s1a7phLN0iaD6-UE7m4qP-z21pH0eSc&q=Eiffel+Tower+Paris+France"
           class="w-100" height="490" style="border:none;"></iframe>
-      </div>
     </div>
   <hr>
   <?php include'footer_2.php'?>
